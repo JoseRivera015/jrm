24ec0b22727eece3ed034a43e17bbbb5ec8ce64f
Jose Rivera <joserivera0015@gmail.com>
2020-11-27 22:13:50 -0500

    msg

--- hisDarkMaterials/scripts/chapters.js
+++ hisDarkMaterials/scripts/chapters.js
@@ -2,5 +2,9 @@ import {cardGrid} from '../../scripts/cardGrid.js'
 let cards = [{title: 'Lyra\'s Jordan',
     picture: 'images/cap1.jpg',
     href: 'cap1.html'
-  }]
+  },
+  {title: 'The City of Magpies',
+  picture: 'images/cap1.jpg',
+  href: 'cap2.html'
+}]
 cardGrid(cards)

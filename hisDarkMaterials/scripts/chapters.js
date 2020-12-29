11cb1154753eada2c8bc8f9fd7883f25d6648851
Jose Rivera <joserivera0015@gmail.com>
2020-12-29 14:24:59 -0500

    mey

--- hisDarkMaterials/scripts/chapters.js
+++ hisDarkMaterials/scripts/chapters.js
@@ -1,26 +1,26 @@
 import {cardGrid} from '../../scripts/cardGrid.js'
 let cards = [{
-    title: 'Lyra\'s Jordan',
+    title: '1. Lyra\'s Jordan',
     picture: 'images/cap1.jpg',
     href: 'cap1.html'
   },
   {
-    title: 'The City of Magpies',
+    title: '9. The City of Magpies',
     picture: 'images/cap9.jpg',
     href: 'cap9.html'
   },
   {
-    title: 'The cave',
+    title: '10. The cave',
     picture: 'images/cap10.jpg',
     href: 'cap10.html'
   },
   {
-    title: 'Theft',
+    title: '11. Theft',
     picture: 'images/cap11.jpg',
     href: 'cap11.html'
   },
   {
-    title: 'Tower of the Angels',
+    title: '12. Tower of the Angels',
     picture: 'images/cap12.jpg',
     href: 'cap12.html'
 }]

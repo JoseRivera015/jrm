4b88a7c852a5055d14a32d43d2eb25402ee63d9c
Jose Rivera <joserivera0015@gmail.com>
2020-12-29 14:22:26 -0500

    not

--- scripts/about.js
+++ scripts/about.js
@@ -2,10 +2,20 @@ let staff = [{title: 'Jose Rivera',
     picture: './images/gallery/yo.jpg',
     features: ['Estudiante en diseño web', 'Estudiante en diseño grafico'],
     about: 'Me considero una persona responsable, optimista, creativa impulsador de metas y desafios prsonales, capaz de vencer los miedos para generar cambios, con capacidad para el trabajo en equipo.',
-    social: ['fb','https://www.facebook.com/']
+    social: ['Facebook','https://www.facebook.com/']
   },
   {title: 'Annie Palacin',
     picture: './images/gallery/annie.jpg',
     features: ['Estudiante en diseño web', 'Estudiante en diseño grafico'],
-    about: 'Me considero una persona responsable, optimista, creativa impulsador de metas y desafios prsonales, capaz de vencer los miedos para generar cambios, con capacidad para el trabajo en equipo.'
+    about: 'Me considero una persona responsable, optimista, creativa impulsador de metas y desafios prsonales, capaz de vencer los miedos para generar cambios, con capacidad para el trabajo en equipo.',
+    social: ['Facebook','https://www.facebook.com/']
   }]
+
+  let staffCard = satff => {
+    let df = document.querySelector('template').cloneNode(/*deep*/true)
+    let cardTitle = df.querySelector('cardTitle')
+    let profilePic = df.querySelector('profilePic')
+    let cardFeatures = df.querySelector('cardFeatures')
+    let cardAbout = df.querySelector('cardAbout')
+    
+  }

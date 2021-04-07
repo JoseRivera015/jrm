f1d5018dff016a876dff5b56b934af4b0ef5955f
Jose Rivera <joserivera0015@gmail.com>
2021-04-07 08:02:01 -0500

    yes

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,23 +1,23 @@
 import {cardGrid} from './cardGrid.js'
 let cards = [
 {title: 'El Catalejo Lacado',
-  picture: './images/gallery/annie.jpg',
+  picture: './images/gallery/yo.jpg',
   href: './theAmberSpyglass/chapters.html'
 },
 {title: 'His Dark Materials',
-picture: './images/gallery/annie.jpg',
+picture: './images/gallery/yo.jpg',
 href: './hisDarkMaterials/chapters.html'
 },
 {title: 'Dark',
-picture: './images/gallery/annie.jpg',
+picture: './images/gallery/yo.jpg',
 href: './dark/chapters.html'
 },
 {title: 'El Catalejo Lacado',
-picture: './images/gallery/annie.jpg',
+picture: './images/gallery/yo.jpg',
 href: './theAmberSpyglass/chapters.html'
 },
 {title: 'El Catalejo Lacado',
-picture: './images/gallery/annie.jpg',
+picture: './images/gallery/yo.jpg',
 href: './theAmberSpyglass/chapters.html'
 }]
 cardGrid(cards)
